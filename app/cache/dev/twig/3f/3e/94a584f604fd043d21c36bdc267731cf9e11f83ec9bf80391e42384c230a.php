c2d31852fee4e26cfdf2c70840a7d1a0bc651f32
barachenadia <barache.nadia@yahoo.fr>
2015-02-20 15:11:26 +0100

    platform d'echange avec symfony 2

---
+++ app/cache/dev/twig/3f/3e/94a584f604fd043d21c36bdc267731cf9e11f83ec9bf80391e42384c230a.php
@@ -0,0 +1,62 @@
+<?php
+
+/* OCPlatformBundle:Advert:index.html.twig */
+class __TwigTemplate_3f3e94a584f604fd043d21c36bdc267731cf9e11f83ec9bf80391e42384c230a extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 2
+        echo "
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <title>Bienvenue sur ma première page avec OpenClassrooms !</title>
+
+    </head>
+
+    <body>
+
+        <h1>Hello World !</h1>
+
+
+
+        <p>
+
+            Le Hello World est un grand classique en programmation.
+
+            Il signifie énormément, car cela veut dire que vous avez
+
+            réussi à exécuter le programme pour accomplir une tâche simple :
+
+            afficher ce hello world !
+
+        </p>
+
+    </body>
+
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "OCPlatformBundle:Advert:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 2,);
+    }
+}
---
+++ app/cache/dev/twig/ae/e9/abba8338321c1aead869c6e983bfcd515abca71b808255cb14bac75c11d7.php
@@ -0,0 +1,34 @@
+<?php
+
+/* OCPlatformBundle:Advert:byebeye.html.twig */
+class __TwigTemplate_aee9abba8338321c1aead869c6e983bfcd515abca71b808255cb14bac75c11d7 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<h1>
+
+    Bye Bye le Monde :-()
+</h1>";
+    }
+
+    public function getTemplateName()
+    {
+        return "OCPlatformBundle:Advert:byebeye.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
